0f5120c91517e63b1e7309472207a7b200c97a01
Bianca Wilkinson <Bianca.Wilkinson@gmail.com>
2020-08-18 02:26:19 +1000

    Index is now responsive

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,34 @@
-import React from "react";
-import Container from "react-bootstrap/Container";
-import Image from "react-bootstrap/Image";
+import React from "react"
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Image from "react-bootstrap/Image"
 
 // Importing componants
-import Layout from '../componants/layout';
+import Layout from "../componants/layout"
 
 const index = () => (
   <Layout>
-       <Image src="images\mmlinedancing-logo.png" alt="Logo" className="mx-auto d-block" />
-  <Container className="text-center">
-    <h1>Line dance classes with Michelle<br />
-    <small class="text-muted">All levels welcome. No dance experience or partners necessary.</small></h1>
+    <Container as="main" className="text-center">
+      <Image
+        src="images\mmlinedancing-logo.png"
+        alt="Logo"
+        className="mx-auto py-5 d-block"
+        fluid
+      />
+      <Row className="px-3">
+        <h3>
+          Line dance classes with Michelle
+          </h3>
+          </Row>
+        <Row>
+          <h4 class="text-muted pt-5">
+            All levels welcome. No dance experience or partners necessary.
+          </h4>
+
+
+      </Row>
     </Container>
   </Layout>
-);
+)
 
-export default index;
+export default index
