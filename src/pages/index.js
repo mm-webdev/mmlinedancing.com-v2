256a5a430a326ad647e3b7c983c82183b73b85ba
Bianca Wilkinson <Bianca.Wilkinson@gmail.com>
2020-08-17 21:42:55 +1000

    Added Bootstrap, Sass & Theme files As per title, also confirmed they are working by loading project in browser

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,9 @@
 import React from "react"
 
+// Importing the Bootstrap CSS created with Themestr.app
+import '../assets/stylesheets/theme.scss';
+import '../assets/stylesheets/theme.css';
+
 export default function Home() {
   return <div>Hello world!</div>
 }
